874f0e28d8e7fd53712215d1bed430f42d24b709
pulchroxloom <dantevbarbieri@gmail.com>
2018-05-29 12:46:53 -0500

    first commit
    Co-Authored-By: Dohinkus <dohinkus@gmail.com>

---
+++ botsu.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class botsu
+{
+	//Robot DT = new Robot();
+	public static void main(String [] args) throws AWTException, InterruptedException
+	{
+		Robot DT = new Robot();
+		
+		smoothMove(0,0,DT);
+		smoothMove(900,1005,DT);
+		Point currentPos = MouseInfo.getPointerInfo().getLocation();
+		double xPos = currentPos.getX();
+		double yPos = currentPos.getY();
+		System.out.print(xPos + " " + yPos);
+		//DT.mouseMove(0,0);
+		//DT.mousePress(InputEvent.BUTTON1_MASK);
+		//DT.mouseMove(200,200);
+		//DT.mouseRelease(InputEvent.BUTTON1_MASK);
+		
+	}
+	
+	public static void smoothMove(int x, int y, Robot DT)
+	{
+		int distance = 
+		DT.mouseMove(x,y);
+	}	
+}
