3aadf8f05776a4a9e585fc768454789620ead8f5
pulchroxloom <dantevbarbieri@gmail.com>
2018-05-31 23:36:08 -0500

    Update minor changes to DVector.java and Botsu.java regarding formatting
    Co-Authored-By: Dohinkus <dohinkus@gmail.com>

--- src/DVector.java
+++ src/DVector.java
@@ -1,6 +1,6 @@
 import java.awt.Point;
 
-public class DVector{
+public class DVector {
   /**
   * @author Dante Barbieri
   * @param x - X Position of the DVector
@@ -15,155 +15,156 @@ public class DVector{
   * @param x - X Position of the DVector
   * @param y - Y Position of the DVector
   */
-  public DVector(double x, double y){
-    this.x = (float)x;
-    this.y = (float)y;
-    a = (float)Math.atan2(y, x);
-    r = (float)Math.sqrt(x * x + y * y);
+  public DVector(double x, double y) {
+    this.x = (float) x;
+    this.y = (float) y;
+    a = (float) Math.atan2(y, x);
+    r = (float) Math.sqrt(x * x + y * y);
   }
 
   /**
   * @author Dante Barbieri
   * @param p - Point containing X and Y Values
   */
-  public DVector(Point p){
-    x = (float)p.getX();
-    y = (float)p.getY();
-    a = (float)Math.atan2(y, x);
-    r = (float)Math.sqrt(x * x + y * y);
+  public DVector(Point p) {
+    x = (float) p.getX();
+    y = (float) p.getY();
+    a = (float) Math.atan2(y, x);
+    r = (float) Math.sqrt(x * x + y * y);
   }
 
   /**
   * @author Dante Barbieri
   * @param v - DVector containing X, Y, A, and R Values
   */
-  public DVector(DVector v){
+  public DVector(DVector v) {
     x = v.x;
     y = v.y;
     a = v.a;
     r = v.r;
   }
 
-  public float x(){
+  public float x() {
     return x;
   }
 
-  public void x(float f){
+  public void x(float f) {
     x = f;
-    a = (float)Math.atan2(y, x);
-    r = (float)Math.sqrt(x * x + y * y);
+    a = (float) Math.atan2(y, x);
+    r = (float) Math.sqrt(x * x + y * y);
   }
 
-  public float y(){
+  public float y() {
     return y;
   }
 
-  public void y(float f){
+  public void y(float f) {
     y = f;
-    a = (float)Math.atan2(y, x);
-    r = (float)Math.sqrt(x * x + y * y);
+    a = (float) Math.atan2(y, x);
+    r = (float) Math.sqrt(x * x + y * y);
   }
 
-  public float a(){
+  public float a() {
     return a;
   }
 
-  public void a(float f){
+  public void a(float f) {
     a = f;
-    x = r * (float)Math.cos(a);
-    y = r * (float)Math.sin(a);
+    x = r * (float) Math.cos(a);
+    y = r * (float) Math.sin(a);
   }
 
-  public float r(){
+  public float r() {
     return r;
   }
 
-  public void r(float f){
+  public void r(float f) {
     r = f;
-    x = r * (float)Math.cos(a);
-    y = r * (float)Math.sin(a);
+    x = r * (float) Math.cos(a);
+    y = r * (float) Math.sin(a);
   }
 
-  public void truncate(float f){
-    if(r > f){
+  public void truncate(float f) {
+    if (r > f) {
       r = f;
-      x = r * (float)Math.cos(a);
-      y = r * (float)Math.sin(a);
+      x = r * (float) Math.cos(a);
+      y = r * (float) Math.sin(a);
     }
   }
 
-  public void changeSpeed(float f){
+  public void changeSpeed(float f) {
     r += f;
-    x = r * (float)Math.cos(a);
-    y = r * (float)Math.sin(a);
+    x = r * (float) Math.cos(a);
+    y = r * (float) Math.sin(a);
   }
 
-  public void add(DVector other){
+  public void add(DVector other) {
     this.x += other.x;
     this.y += other.y;
-    a = (float)Math.atan2(y, x);
-    r = (float)Math.sqrt(x * x + y * y);
+    a = (float) Math.atan2(y, x);
+    r = (float) Math.sqrt(x * x + y * y);
   }
 
-  public void sub(DVector other){
+  public void sub(DVector other) {
     this.x -= other.x;
     this.y -= other.y;
-    a = (float)Math.atan2(y, x);
-    r = (float)Math.sqrt(x * x + y * y);
+    a = (float) Math.atan2(y, x);
+    r = (float) Math.sqrt(x * x + y * y);
   }
 
-  public void mult(float f){
+  public void mult(float f) {
     r *= f;
-    x = r * (float)Math.cos(a);
-    y = r * (float)Math.sin(a);
+    x = r * (float) Math.cos(a);
+    y = r * (float) Math.sin(a);
   }
 
-  public void div(float f){
+  public void div(float f) {
     r /= f;
-    x = r * (float)Math.cos(a);
-    y = r * (float)Math.sin(a);
+    x = r * (float) Math.cos(a);
+    y = r * (float) Math.sin(a);
   }
 
-  public float dist(DVector other){
+  //TODO: Fix this shit
+
+  public float dist(DVector other) {
     return Math.abs(DVector.sub(this, other).r);
   }
 
-  public DVector copy(){
+  public DVector copy() {
     return new DVector(this);
   }
 
-  public static DVector add(DVector a, DVector b){
-    return new DVector(a.x+b.x, a.y+b.y);
+  public static DVector add(DVector a, DVector b) {
+    return new DVector(a.x + b.x, a.y + b.y);
   }
 
-  public static DVector sub(DVector a, DVector b){
-    return new DVector(a.x-b.x, a.y-b.y);
+  public static DVector sub(DVector a, DVector b) {
+    return new DVector(a.x - b.x, a.y - b.y);
   }
 
-  public static DVector mult(DVector v, float f){
+  public static DVector mult(DVector v, float f) {
     DVector a = v.copy();
     a.mult(f);
     return a;
   }
 
-  public static DVector div(DVector v, float f){
+  public static DVector div(DVector v, float f) {
     DVector a = v.copy();
     a.div(f);
     return a;
   }
 
-  public static DVector random(float mag){
-    float angle = (float)(Math.random() * Math.PI*2);
+  public static DVector random(float mag) {
+    float angle = (float) (Math.random() * Math.PI * 2);
     return new DVector(mag * Math.cos(angle), mag * Math.sin(angle));
   }
 
-  public static float dist(DVector a, DVector b){
+  public static float dist(DVector a, DVector b) {
     return Math.abs(DVector.sub(a, b).r);
   }
 
   @Override
-  public String toString(){
-    // return "(x, y): (" + x + ", " + y + ")\n(r, \u03b8): (" + r + ", " + a + ")";
+  public String toString() {
     return "(x, y): (" + x + ", " + y + ")\n(r, θ): (" + r + ", " + a + ")";
   }
 }
--- src/botsu.java
+++ src/botsu.java
@@ -3,22 +3,29 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Botsu
-{
+public class Botsu {
 	public static Robot DT;
 	public static DVector position;
 	public static DVector velocity;
+	public static float pxCircleSize;
+	public static float sliderSpeed;
 
-	public static void main(String [] args) throws AWTException, InterruptedException
-	{
+	public static void main(String[] args) throws AWTException, InterruptedException {
 		DT = new Robot();
+
+		double[] difficulty = FileDataReader.readDifficulties(args[0]); //args[0] represents the filename passed into the program through the console
+
+		pxCircleSize = 54.4f - 4.48f * (float) difficulty[0];
+		sliderSpeed = 100f * (float) difficulty[3];
+
+		OsuObject[] objects = FileDataReader.getObjects(args[0]);
 
 		position = new DVector(getMousePos());
 		velocity = new DVector(0, 0);
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
-		for (int i = 0; i < 10; i++){
+		for (int i = 0; i < 10; i++) {
 			DVector goal = new DVector(Math.random() * screenSize.getWidth(), Math.random() * screenSize.getHeight());
 
 			System.out.println("Goal:\n" + goal);
@@ -29,35 +36,33 @@ public class Botsu
 		}
 	}
 
-	public static void smoothMove(int x, int y, int smoothness)
-	{
+	public static void smoothMove(int x, int y, int smoothness) {
 		DVector start = new DVector(getMousePos());
 		DVector current = start.copy();
 		DVector cease = new DVector(x, y);
 		DVector diff = DVector.sub(cease, start);
 		diff.div(smoothness);
-		for(int i = 0; i < smoothness; i++){
+		for (int i = 0; i < smoothness; i++) {
 			current.add(diff);
-			DT.mouseMove((int)current.x(),(int)current.y());
+			DT.mouseMove((int) current.x(), (int) current.y());
 		}
 	}
 
-	public static void smoothMove(DVector cease, int smoothness)
-	{
+	public static void smoothMove(DVector cease, int smoothness) {
 		DVector start = new DVector(getMousePos());
 		DVector current = start.copy();
 		DVector diff = DVector.sub(cease, start);
 		diff.div(smoothness);
-		for(int i = 0; i < smoothness; i++){
+		for (int i = 0; i < smoothness; i++) {
 			current.add(diff);
-			DT.mouseMove((int)current.x(),(int)current.y());
+			DT.mouseMove((int) current.x(), (int) current.y());
 		}
 	}
 
 	public static void steerMove(DVector target, float radius, float max_speed, float slowing_distance, float max_force) {
 		DVector target_offset, desired_velocity, steering, acceleration;
 		float distance, ramped_speed, clipped_speed;
-		while(DVector.dist(position, target) > radius){
+		while (DVector.dist(position, target) > radius) {
 			target_offset = DVector.sub(target, position);
 			distance = target_offset.r();
 			ramped_speed = max_speed * (distance / slowing_distance);
@@ -67,28 +72,32 @@ public class Botsu
 			acceleration = steering.copy();
 			acceleration.truncate(max_force);
 			velocity.add(acceleration);
-			if(random() < 0.003 * max_speed) velocity.changeSpeed(random(-0.15f * max_speed, 0.1f * max_speed));
+			if (random() < 0.003 * max_speed)
+				velocity.changeSpeed(random(-0.15f * max_speed, 0.1f * max_speed));
 			velocity.truncate(max_speed);
 			position.add(velocity);
-			if(random() < 0.3 * max_speed) position.add(DVector.random(1.5f * max_speed));
-			DT.mouseMove((int)position.x(),(int)position.y());
+			if (random() < 0.3 * max_speed)
+				position.add(DVector.random(1.5f * max_speed));
+			DT.mouseMove((int) position.x(), (int) position.y());
 		}
 	}
 
-	public static Point getMousePos(){ return MouseInfo.getPointerInfo().getLocation();	}
-
-	public final static float random(){
-		return (float)Math.random();
+	public static Point getMousePos() {
+		return MouseInfo.getPointerInfo().getLocation();
 	}
 
-	public final static float random(float high){
-		return high * (float)Math.random();
+	public final static float random() {
+		return (float) Math.random();
 	}
 
-	public final static float random(float low, float high){
+	public final static float random(float high) {
+		return high * (float) Math.random();
+	}
+
+	public final static float random(float low, float high) {
 		float temp = low < high ? high : low;
 		low = low < high ? low : high;
 		high = temp;
-		return (high - low) * (float)Math.random() + low;
+		return (high - low) * (float) Math.random() + low;
 	}
 }
